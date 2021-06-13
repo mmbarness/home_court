8d5a76aaaf9f6946bd077a231a51754e3312aa34
matt2yu <matt2yu@gmail.com>
2021-06-13 17:55:56 -0500

    message branch test

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const app = express();
+const mongoose = require('mongoose');
+const db = require('./backend/config/keys').mongoURI;
 
 app.get("/", (req, res) => res.send("Backend setup working"));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));

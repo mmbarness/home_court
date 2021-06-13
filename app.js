6e168ab5340df79d87b8eadcc65492640ca7811e
matt2yu <matt2yu@gmail.com>
2021-06-13 17:44:37 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("Backend setup working"));
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));

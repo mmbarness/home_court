8154134bc53481ff719626954320dea37f64997f
matthew barnes <011mbarnes@gmail.com>
2021-06-14 17:38:22 -0400

    addt'l nav bar stuff'

--- frontend/src/components/nav/navbar_container.jsx
+++ frontend/src/components/nav/navbar_container.jsx
@@ -4,7 +4,9 @@ import { logout } from '../../actions/session_actions';
 import NavBar from './navbar';
 
 const mapStateToProps = state => ({
-  loggedIn: state.session.isAuthenticated
+  loggedIn: state.session.isAuthenticated,
+  currentUser: {username: 'placeholder_username', email: 'placeholder_email'}
+    // state.session.user
 });
 
 export default connect(

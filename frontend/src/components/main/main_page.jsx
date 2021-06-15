1b01f729f0dae993b6e068f2b152fa2df895f7e9
will-ku <william.ku9192@gmail.com>
2021-06-15 13:33:05 -0400

    created css file for main page

--- frontend/src/components/main/main_page.jsx
+++ frontend/src/components/main/main_page.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 
 class MainPage extends React.Component {
-
   render() {
-    return <div>main</div>;
+    return (
+      <div className="main-page">
+        <section className="main-page-events"> event cards go here </section>
+        <section className="main-page-map"> google map goes here</section>
+      </div>
+    );
   }
 }
 

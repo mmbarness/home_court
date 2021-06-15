9bf3c9e5b84915bfd8002a1ae252465b3f48c844
moustafa <mogarcia626@gmail.com>
2021-06-15 16:50:14 -0400

    Set pins on map

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -1,12 +1,40 @@
 import React from 'react';
 import EventMap from '../map/event_map'
 
+const eventMarkers = [
+  {sport: 'basketball',
+    lat: 40.715128,
+    lng: -73.9370077
+  },
+  {sport: 'soccer',
+    lat: 40.7130519,
+    lng: -73.908111
+  },
+  {sport: 'volleyball',
+    lat: 40.715128,
+    lng: -73.9370077
+  },
+  {sport: 'basketball',
+    lat: 40.698152,
+    lng: -73.9187878
+  },
+  {sport: 'soccer',
+    lat: 40.7199784,
+    lng: -73.9535401
+  },
+  {sport: 'volleyball',
+    lat: 40.7299653,
+    lng: -73.9157633
+  },
+]
+
 class MainPage extends React.Component {
 
   render() {
+    console.log(eventMarkers)
     return (
       <div>
-        <EventMap />
+        <EventMap eventMarkers={eventMarkers}/>
         <footer>
           Copyright &copy; 2021 matty & the boyz
         </footer>
--- frontend/src/components/map/event_map.jsx
+++ frontend/src/components/map/event_map.jsx
@@ -5,24 +5,28 @@ import {
     Marker,
     InfoWindow,
 } from '@react-google-maps/api';
+import mapStyles from './map_styles';
 // import ReactDOM from 'react-dom';
 // import { withRouter } from 'react-router-dom';
 // import MarkerManager from '../../util/marker_manager';
 
 const libraries = ['places']
-
 const mapContainerStyle = {
     width: "45vw",
     height: "60vh",
 }
+const options = {
+    styles: mapStyles,
+    disableDefaultUI: true,
+    zoomControl: true
+}
 
-//Manhattan
+function EventMap(props) {
 
-function EventMap() {
-
+    //Brooklyn
     const center = {
-        lat: 40.783058,
-        lng: -73.971252,
+        lat: 40.701000,
+        lng: -73.941011
     }
 
     const { isLoaded, loadError } = useLoadScript({
@@ -30,26 +34,44 @@ function EventMap() {
         libraries: ['places']
     })
 
-    // const options = {
-    //     styles: ,
-
-    // }
-
     if (loadError) return 'Error loading maps';
     if (!isLoaded) return 'Loading the map';
-
+    console.log(props.eventMarkers)
     return (
-        <GoogleMap
-            mapContainerStyle={mapContainerStyle}
-            center={center}
-            zoom={14}
-        >
-            { /* Child components, such as markers, info windows, etc. */}
-            <></>
-        </GoogleMap>
+        <div>
+            <h1>Home Court <span role='img' aria-label='ball'>🤾</span></h1>
+            <GoogleMap
+                mapContainerStyle={mapContainerStyle}
+                center={center}
+                zoom={14}
+                options={options}
+                onClick={(event) => {
+                    console.log(event)
+                }}
+            >
+                {props.eventMarkers.map((event, i) => (
+                        <Marker key={i} position={{ lat: event.lat, lng: event.lng }} />
+                ))}
+                
+                <></>
+            </GoogleMap>
+        </div>
     )
 }
 
 
 export default EventMap
+
+
+{/* <GoogleMap>
+    <Marker
+    key={Marker.time.toISOString()}
+    position={{lat: Marker.lat, lng: Marker.lng}}
+    icon={{
+        url: './icon.svg',
+        scaledSize: new window.google.maps.Size(30,30),
+        origin: new window.google.maps.point(0,0),
+
+    }}
+</GoogleMap> */}
 

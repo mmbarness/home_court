21bf777f2bbad1e1fff2d5e2ab0cfb69387bd16f
matthew barnes <011mbarnes@gmail.com>
2021-06-16 10:14:50 -0400

    finally working

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(passport.initialize());
 require('./backend/config/passport')(passport);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-// app.get("/", (req, res) => res.send("Backend setup working"));
+
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
@@ -24,7 +24,7 @@ mongoose
 const path = require('path');
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('frontend/build'));
-  app.get('/', (req, res) => {
+  app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build','/index.html'));
   })
 }

80968a70fa5fa3856b40597e81f2f824f8e718b0
matthew barnes <011mbarnes@gmail.com>
2021-06-16 10:00:53 -0400

    maybe this works

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ mongoose
 
 const path = require('path');
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('frontend/build'));
+  app.use(path.join(__dirname, 'frontend/build'));
   app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build','/index.html'));
   })

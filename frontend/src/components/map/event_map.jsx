f58f181039ae70e57527db517dadbc76b7dab882
Moustafa Garcia <mogarcia626@gmail.com>
2021-06-17 10:48:36 -0400

    Revert "Event form"

--- frontend/src/components/map/event_map.jsx
+++ frontend/src/components/map/event_map.jsx
@@ -10,8 +10,8 @@ import mapStyles from './map_styles';
 
 const libraries = ['places']
 const mapContainerStyle = {
-    width: "45vw",
-    height: "90vh",
+    width: "80vw",
+    height: "80vh",
 }
 const options = {
     styles: mapStyles,
@@ -26,9 +26,7 @@ function EventMap(props) {
         libraries: libraries
     })
 
-    const [selected, setSelected] = React.useState(null) // selected is a current event whose infow window is open     
-    const [creatingEvent, setCreatingEvent] = React.useState(false) //true: waiting for pin to drop to create event
-    const [eventLocation, setEventLocation] = React.useState(null) //evenLocation are the coordinates of a new game
+    const [selected, setSelected] = React.useState(null)    
 
     const mapRef = React.useRef();
     const onMapLoad = React.useCallback((map) => {
@@ -37,16 +35,8 @@ function EventMap(props) {
 
     const panTo = React.useCallback(({lat, lng}) => {
         mapRef.current.panTo({lat, lng});
-        mapRef.current.setZoom(15);
+        mapRef.current.setZoom(14);
     }, []);
-<<<<<<< HEAD
-
-        // const eventsArr = Object.values(props.events.all);
-   console.log(props)
-    if (loadError) return 'Error loading maps';
-    if (!isLoaded) return 'Loading the map';
-    
-=======
     console.log(props.events)
     const eventsArr = Object.values(props.events);
    
@@ -55,64 +45,21 @@ function EventMap(props) {
     console.log(eventsArr[0].lat);
     console.log(parseFloat(eventsArr[0].lat));
 
->>>>>>> c2f0a80bb05a722bcae3a07a11698213a8f0cebf
     return (
         <div>
             <h1>Where are the games happening? <span role='img' aria-label='ball'>🤾</span></h1>
-
-            {creatingEvent ?
-                <button id="create game" onClick={() => {
-                    setCreatingEvent(false)
-                    setEventLocation(null)
-                }}>
-                    Cancel</button>
-                : 
-                <button id="create game" onClick={() => setCreatingEvent(true)}>Create Game</button> }
-
             <ResetMapButton panTo={panTo} center={props.center} />
-
             <GoogleMap
                 onLoad={onMapLoad}
                 mapContainerStyle={mapContainerStyle}
                 center={props.center}
-                zoom={15}
+                zoom={14}
                 options={options}
-                onClick={(e) => {
-                    if (selected) setSelected(null)                    
-                    if (creatingEvent) {
-                        setEventLocation({
-                            lat: e.latLng.lat(),
-                            lng: e.latLng.lng(),
-                        })
-                    }
+                onClick={() => {
+                    if (selected) setSelected(null)
                 }}
             >
-<<<<<<< HEAD
-
-            {eventLocation ? (
-                <div>
-                    <Marker position={eventLocation} />
-
-                    <InfoWindow 
-                        position={eventLocation}
-                            options={ {pixelOffset: new window.google.maps.Size(0,-46)}}
-                        onCloseClick={() => null}
-                    >
-                    <button onClick={() => 
-                        props.openModal({
-                            modal: 'event-form',
-                            data: eventLocation
-                        }) 
-                    }>Create Game Here</button>
-
-                    </InfoWindow> 
-                </div> ) : null
-            }
-
-            {props.events.map((event, i) => (
-=======
             {eventsArr.map((event, i) => (
->>>>>>> c2f0a80bb05a722bcae3a07a11698213a8f0cebf
                 <Marker key={i}
                     position={{ lat: parseFloat(event.lat), lng: parseFloat(event.lng)}}                        
                     icon={{
@@ -138,13 +85,12 @@ function EventMap(props) {
                     <div>
                         <h1>{selected.title}</h1>
                             <p>Start: {selected.startDate.toLocaleTimeString()}</p>
-                            <p>End: {selected.endDate.toLocaleTimeString()}</p>                      
+                            <p>End: {selected.endDate.toLocaleTimeString()}</p>
+                        
+
                     </div>
                 </InfoWindow>): null }
             </GoogleMap>
-
-              
-
         </div>
     )
 }
@@ -152,10 +98,10 @@ function EventMap(props) {
 export default EventMap
 
 //Brooklyn
-// const brooklyn = {
-//     lat: 40.701000,
-//     lng: -73.941011
-// }
+const brooklyn = {
+    lat: 40.701000,
+    lng: -73.941011
+}
 
 function selectIcon(sport) {
     switch (sport) {
@@ -176,11 +122,4 @@ function selectIcon(sport) {
 //     return <button><img src='volleyball.svg' alt='center' /></button>
 // }
 
-// {eventFormModal ? 
-//                 <EventFormModal
-//                     className="modal-background"
-//                     onClick={() => setEventFormModal(false)}
-//                     setEventFormModal={setEventFormModal}
-//                     eventFormModal={() => eventFormModal}
-//                     eventLocation={() => eventLocation}
-//                 /> : null } 
+

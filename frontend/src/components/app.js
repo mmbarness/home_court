c3681811adc5e4a65a81983931d15297923b0207
will-ku <william.ku9192@gmail.com>
2021-06-17 13:56:39 -0400

    changes to frontend

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { AuthRoute, ProtectedRoute } from "../util/route_util";
 import { Switch } from "react-router-dom";
-import { Route, Redirect, withRouter } from 'react-router-dom';
+import { Route, Redirect, withRouter } from "react-router-dom";
 
 import MainPageContainer from "../components/main/main_page_container";
 import LoginFormContainer from "./session/login_form_container";
@@ -9,11 +9,12 @@ import SignupFormContainer from "./session/signup_form_container";
 import ProfileContainer from "./profile/profile_container";
 import Footer from "./footer";
 import Modal from "../components/modal/modal.jsx";
+import { NavBarHook } from "./nav/navbar_hook";
+import SplashPage from "./splash";
+import "../style/css/reset.css";
 
 import "../style/css/application.css";
-import { NavBarHook } from "./nav/navbar_hook";
 import "../style/css/modal.css";
-import SplashPage from "./splash";
 
 const App = () => (
   <div className="top">
@@ -21,11 +22,11 @@ const App = () => (
       <Modal />
       <NavBarHook />
       <Switch>
-        <ProtectedRoute exact path="/" component={MainPageContainer} />             
+        <ProtectedRoute exact path="/" component={MainPageContainer} />
         <AuthRoute exact path="/login" component={LoginFormContainer} />
         <AuthRoute exact path="/signup" component={SignupFormContainer} />
         <ProtectedRoute exact path="/profile" component={ProfileContainer} />
-        <Route exact path="/splash" component={SplashPage} />  
+        <Route exact path="/splash" component={SplashPage} />
       </Switch>
     </div>
     <Footer />

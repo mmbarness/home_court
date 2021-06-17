bc22a68bfc597aafeffeefdfb4d92da9949a07c9
matthew barnes <011mbarnes@gmail.com>
2021-06-17 13:51:24 -0400

    yep

--- frontend/src/components/events/event_show_container.js
+++ frontend/src/components/events/event_show_container.js
@@ -3,7 +3,7 @@ import EventShow from "./event_show";
 import { closeModal } from "../../actions/modal_actions";
 
 const mapStateToProps = (state) => ({
-  event: state.ui.modal.event,
+  event: state.ui.modal.data,
 });
 
 const mapDispatchToProps = (dispatch) => ({
--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -1,5 +1,5 @@
 import React from "react";
-import EventMap from "../map/event_map";
+import EventMapContainer from "../map/event_map_container";
 import EventsIndex from "../events/events_index.jsx";
 import "../../style/css/main_page.css";
 import "../../style/css/events.css";
@@ -40,9 +40,9 @@ class MainPage extends React.Component {
           />
         </section>
         <section className="main-page-map">
-          <EventMap
+          <EventMapContainer
           center={this.state.center}
-          events={this.props.events}/>
+          events={eventMarkers}/>
         </section>
       </div>
     );

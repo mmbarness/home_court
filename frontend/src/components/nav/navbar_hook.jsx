d1ad426bfe802b4587ec2e4c9482a8550cab6f19
matthew barnes <011mbarnes@gmail.com>
2021-06-17 23:20:23 -0400

    style stuff

--- frontend/src/components/nav/navbar_hook.jsx
+++ frontend/src/components/nav/navbar_hook.jsx
@@ -20,23 +20,22 @@ export const NavBarHook = () => {
                 <Link id="profile-link" to={'/profile'}>{currentUser.username}</Link>
                 <Link to={'/team'}>About the Team</Link>
                 <button id="logout-button" onClick={logoutUser}>Logout</button>
-                <button id="create game" onClick={null}>Create Game</button>
             </div>
         );
     }
     const notLoggedInLinks = () => {
         return (
             <div className="Links2 limit-min-max">
-                <Link to={'/team'}>About the Team</Link>
-                <Link to={'/signup'}>Signup</Link>
-                <Link to={'/login'}>Login</Link>
+                <Link to={'/team'} className="limit-min-max">About the Team</Link>
+                <Link to={'/signup'} className="limit-min-max">Signup</Link>
+                <Link to={'/login'} className="limit-min-max">Login</Link>
             </div>
         );
     }
 
   return (
     <div className="nav-bar">
-      <h1 id="header-home-court-text">
+      <h1 id="header-home-court-text" className="limit-min-max">
         <Link to={"/"}>
           {/* <img
             src={require("../images/logowhitebg.png")}

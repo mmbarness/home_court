35131fb753819b36ba9c392566b02fe40e67f903
will-ku <william.ku9192@gmail.com>
2021-06-18 17:08:04 -0400

    updated navbar styling

--- frontend/src/components/nav/navbar_hook.jsx
+++ frontend/src/components/nav/navbar_hook.jsx
@@ -7,37 +7,54 @@ import "../../style/css/navbar.scss";
 export const NavBarHook = () => {
   let loggedIn = useSelector((state) => state.session.isAuthenticated);
   let currentUser = useSelector((state) => state.session.user);
- 
-    const dispatch = useDispatch();
-    const logoutUser = (e) => {
-      e.preventDefault();
-      (dispatch(logout()))
-    }
 
-    const loggedInLinks = () => {
-        return (
-            <div className="Links1 limit-min-max">
-                <Link id="profile-link" to={'/profile'}>{currentUser.username}</Link>
-                <Link to={'/team'}>About the Team</Link>
-                <button id="logout-button" onClick={logoutUser}>Logout</button>
-            </div>
-        );
-    }
-    const notLoggedInLinks = () => {
-        return (
-            <div className="Links2 limit-min-max">
-                <Link to={'/team'} className="limit-min-max">About the Team</Link>
-                <Link to={'/signup'} className="limit-min-max">Signup</Link>
-                <Link to={'/login'} className="limit-min-max">Login</Link>
-            </div>
-        );
-    }
+  const dispatch = useDispatch();
+  const logoutUser = (e) => {
+    e.preventDefault();
+    dispatch(logout());
+  };
+
+  const loggedInLinks = () => {
+    return (
+      <div className="Links1 limit-min-max">
+        <Link id="profile-link" to={"/profile"}>
+          Welcome {currentUser.username}!
+        </Link>
+        <Link to={"/team"} id="profile-link">
+          About the Team
+        </Link>
+        <a href="#" id="profile-link" onClick={logoutUser}>
+          Logout
+        </a>
+      </div>
+    );
+  };
+  const notLoggedInLinks = () => {
+    return (
+      <div className="Links2 limit-min-max">
+        <Link to={"/team"} className="limit-min-max">
+          About the Team
+        </Link>
+        <Link to={"/signup"} className="limit-min-max">
+          Signup
+        </Link>
+        <Link to={"/login"} className="limit-min-max">
+          Login
+        </Link>
+      </div>
+    );
+  };
 
   return (
     <div className="nav-bar">
       <h1 id="header-home-court-text" className="limit-min-max">
         <Link to={"/"}>
-          Home Court
+          <img
+            id="logo"
+            src="https://pixsy-dev.s3.us-east-2.amazonaws.com/homecourtlogo3.png"
+            alt="Home Court"
+            height="65px"
+          />
         </Link>
       </h1>
       {loggedIn ? loggedInLinks() : notLoggedInLinks()}

6806cbedde1b335a7f96727ed93dc4a2d01b949a
moustafa <mogarcia626@gmail.com>
2021-06-18 16:54:47 -0400

    add modal, fix styling bugs

--- frontend/src/components/events/event_index_item.jsx
+++ frontend/src/components/events/event_index_item.jsx
@@ -9,6 +9,10 @@ class EventIndexItem extends React.Component {
   constructor(props) {
     super(props);
     this.openEventModal = this.openEventModal.bind(this);
+    // this.joinEventButton = this.joinEventButton.bind(this);
+    // this.unjoinEventButton = this.unjoinEventButton.bind(this);
+    // this.joinedOrNot = this.joinedOrNot.bind(this);
+    // this.unJoin = this.unJoin.bind(this);
   }
 
   openEventModal() {
@@ -18,13 +22,52 @@ class EventIndexItem extends React.Component {
     });
   }
 
+  // joinedOrNot() {
+  //   let attendees = this.props.event.attendees;
+  //   let curUser = this.props.currentUser;
+  //   let bool;
+  //   if (!_.isEmpty(attendees)) {
+  //     bool = attendees.some((user) => user.username === curUser.username)
+  //       ? true
+  //       : false;
+  //   }
+  //   return bool;
+  // }
+
+  // joinEventButton() {
+  //   return (
+  //     <button
+  //       className="join-event event-item-button"
+  //       onClick={() => {
+  //         this.setState({ clicked: !this.state.clicked });
+  //         this.props.joinEvent(this.props.event._id, this.props.currentUser);
+  //       }}
+  //     >
+  //       Join Game
+  //     </button>
+  //   );
+  // }
+
+  // unJoin(e){
+  //   e.preventDefault();
+  //   let obj = {user_id: this.props.currentUser.id, event_id: this.props.event._id}
+  //   this.setState({ clicked: !this.state.clicked });
+  //   this.props.unJoinEvent(obj)
+  // }
+
+  // unjoinEventButton() {
+  //   return (
+  //     <button onClick={this.unJoin} className="unjoin-event event-item-button">✔ Attending</button>
+  //   );
+  // }
+
   render() {
     const { event } = this.props;
     return (
       <li className="event-index-item">
         <div className="event-item-content" onClick={this.openEventModal}>
           <h1 className="event-item-header">
-            <h1 className="event-item-title">{event.title}</h1>
+            <h2 className="event-item-title">{event.title}</h2>
             <h2 className="event-item-sport">
               {displaySportWithEmoji(event.sport)}
             </h2>
@@ -35,6 +78,9 @@ class EventIndexItem extends React.Component {
           <div></div>
         </div>
         <div className="event-item-footer">
+          {/* {this.state.clicked
+            ? this.unjoinEventButton()
+            : this.joinEventButton()} */}
             <JoinOrNot event={event}/>
         </div>
       </li>

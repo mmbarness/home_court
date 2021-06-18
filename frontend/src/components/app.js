2da0d7124eeb2b19314ec15a89983a35b2f77a83
will-ku <william.ku9192@gmail.com>
2021-06-17 20:31:25 -0400

    merging main

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -7,11 +7,9 @@ import MainPageContainer from "../components/main/main_page_container";
 import LoginFormContainer from "./session/login_form_container";
 import SignupFormContainer from "./session/signup_form_container";
 import ProfileContainer from "./profile/profile_container";
-import Profile from './profile/profile'
-import Footer from "./footer";
+import Profile from "./profile/profile";
 import Modal from "../components/modal/modal.jsx";
 import { NavBarHook } from "./nav/navbar_hook";
-import SplashPage from "./splash";
 // import "../style/css/reset.css";
 
 import "../style/css/application.css";
@@ -29,8 +27,8 @@ const App = () => (
         <AuthRoute exact path="/login" component={LoginFormContainer} />
         <AuthRoute exact path="/signup" component={SignupFormContainer} />
         <ProtectedRoute exact path="/profile" component={Profile} />
-        <Route exact path="/splash" component={SplashPage} />  
-        <Route exact path="/team" component={Team} />  
+        <Route exact path="/splash" component={SplashPage} />
+        <Route exact path="/team" component={Team} />
       </Switch>
     </div>
   </div>

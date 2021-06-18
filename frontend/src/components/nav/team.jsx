039c0be606a532e81e2fd788a16b9d38b65aad02
matt2yu <matt2yu@gmail.com>
2021-06-18 11:40:16 -0500

    asdf

--- frontend/src/components/nav/team.jsx
+++ frontend/src/components/nav/team.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
-// import '../../style/css/navbar.css'
+import '../../style/css/team.scss'
 
 export const Team = () => {
   return (

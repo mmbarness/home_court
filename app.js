3950c6eefbef6c35834dd4686c778c7daa54aefd
matthew barnes <011mbarnes@gmail.com>
2021-06-21 11:56:40 -0400

    tryna fix heroku

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ require('./backend/config/passport')(passport);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
@@ -23,7 +22,7 @@ mongoose
 const path = require('path');
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('frontend/build'));
-  app.get('*', (req, res) => {
+  app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build','index.html'));
   })
 }

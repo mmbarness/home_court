b7618b203ed6abbe0056b64acb3a497fe2cb0bfe
moustafa <mogarcia626@gmail.com>
2021-06-21 12:17:23 -0400

    2nd try

---
+++ frontend/src/actions/map_actions.js
@@ -0,0 +1,12 @@
+// export const FILTER_BY_MAP_BOUNDS = "FILTER_BY_MAP_BOUNDS";
+// export const CLOSE_MODAL = "CLOSE_MODAL";
+
+// export const filterBounds = (mapBounds) => ({
+//   type: FILTER_BY_MAP_BOUNDS,
+//   minLat,
+//   data: modal.data,
+// });
+
+// export const filterActivity = (activityArr) => ({
+//   type: CLOSE_MODAL,
+// });

a9484b6a5df37446b35534aa48427eccc242ad71
will-ku <william.ku9192@gmail.com>
2021-07-07 15:17:37 -0400

    removed debugger from session reducer

--- frontend/src/components/profile/profile.jsx
+++ frontend/src/components/profile/profile.jsx
@@ -1,38 +1,39 @@
-import React, { useState, useEffect } from 'react';
-import { useDispatch, useSelector} from "react-redux";
-import { Link } from 'react-router-dom'
-import {fetchUserEvents} from '../../actions/session_actions'
-import EventIndexItemContainer from '../events/event_index_item_container';
-import * as _ from 'underscore'
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { fetchUserEvents } from "../../actions/session_actions";
+import EventIndexItemContainer from "../events/event_index_item_container";
+import * as _ from "underscore";
 
 export const Profile = () => {
+  const currentUser = useSelector((state) => state.session.user);
+  const allEvents = useSelector((state) => state.events);
+  // const userEvents = useSelector((state) => state.session.user.events);
+  const dispatch = useDispatch();
 
-    const currentUser = useSelector((state) => state.session.user)
-    const allEvents = useSelector(state => state.events)
-    const userEvents = useSelector(state => state.session.user.events)
-    const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchUserEvents(currentUser.id));
+  }, [allEvents]);
 
-    useEffect(() => {
-        dispatch(fetchUserEvents(currentUser.id))
-    },[allEvents])
-
-    const renderEvents = () => {
-        let events = currentUser.events
-        if (!_.isEmpty(events)) {
-            return (<div className="user-prof-events">
-                {events.data.map((event, i) => (
-                    <EventIndexItemContainer key={i} event={event} />
-                ))}
-            </div>)
-        }
-    }      
-
-    return(
-        <div className="user-prof-events-container">
-            <h1>Your upcoming games</h1>
-            {renderEvents()}
+  const renderEvents = () => {
+    let events = currentUser.events;
+    if (!_.isEmpty(events)) {
+      return (
+        <div className="user-prof-events">
+          {events.data.map((event, i) => (
+            <EventIndexItemContainer key={i} event={event} />
+          ))}
         </div>
-    )
-}
+      );
+    }
+  };
 
-export default Profile
+  return (
+    <div className="user-prof-events-container">
+      <h1>Your upcoming games</h1>
+      {renderEvents()}
+    </div>
+  );
+};
+
+export default Profile;
--- frontend/src/reducers/session_reducer.js
+++ frontend/src/reducers/session_reducer.js
@@ -1,35 +1,37 @@
-import { RECEIVE_CURRENT_USER, 
-         RECEIVE_USER_LOGOUT, 
-         RECEIVE_USER_SIGN_IN, 
-         RECEIVE_USER_EVENTS} from '../actions/session_actions';
+import {
+  RECEIVE_CURRENT_USER,
+  RECEIVE_USER_LOGOUT,
+  RECEIVE_USER_SIGN_IN,
+  RECEIVE_USER_EVENTS,
+} from "../actions/session_actions";
 
 const initialState = {
   isAuthenticated: false,
-  user: {}
+  user: {},
 };
 
-export default function(state = initialState, action) {
+export default function (state = initialState, action) {
   const nextState = Object.assign({}, state);
   switch (action.type) {
     case RECEIVE_CURRENT_USER:
       return {
         ...state,
         isAuthenticated: !!action.currentUser,
-        user: action.currentUser
+        user: action.currentUser,
       };
     case RECEIVE_USER_LOGOUT:
       return {
         isAuthenticated: false,
-        user: undefined
+        user: undefined,
       };
     case RECEIVE_USER_SIGN_IN:
       return {
         ...state,
-        isSignedIn: true
+        isSignedIn: true,
       };
     case RECEIVE_USER_EVENTS:
-      let curUserUsername = state.user.username
-      nextState.user.events = action.events
+      let curUserUsername = state.user.username;
+      nextState.user.events = action.events;
       return nextState;
     default:
       return state;

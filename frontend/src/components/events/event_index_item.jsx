cb116afaabd5abc2a4ac2f5c377c8ce5f7c78156
will-ku <william.ku9192@gmail.com>
2021-08-31 12:33:29 -0400

    clicking join game and unjoin game will no longer open modal

--- frontend/src/components/events/event_index_item.jsx
+++ frontend/src/components/events/event_index_item.jsx
@@ -3,47 +3,56 @@ import { formatDateTime } from "../../util/date_util_short";
 import JoinOrNot from "./joinOrNot";
 import { displaySportWithEmoji } from "../../util/sport_emoji";
 
-
 class EventIndexItem extends React.Component {
   constructor(props) {
     super(props);
     this.openEventModal = this.openEventModal.bind(this);
   }
 
-  openEventModal() {
+  openEventModal(e) {
     this.props.openModal({
       modal: "event-show",
       data: {
         startLocation: this.props.center,
         event: this.props.event,
-      }
+      },
     });
   }
 
   render() {
     const { event } = this.props;
-    if ((event !== null) && (event.visible)){
+    if (event !== null && event.visible) {
       return (
-        <li className="event-index-item" onClick={this.openEventModal}>
-          <div className="event-item-content">
+        <li className="event-index-item">
+          <div
+            className="event-item-content"
+            onClick={(e) => this.openEventModal(e)}
+          >
             <div className="event-item-header">
               <h1 className="event-item-title">{event.title}</h1>
               <h2 className="event-item-sport">
                 {displaySportWithEmoji(event.sport)}
               </h2>
             </div>
-            <p className="event-item-date">
+            <p
+              className="event-item-date"
+              onClick={(e) => this.openEventModal(e)}
+            >
               {formatDateTime(this.props.event.startDate)}
             </p>
-            <div></div>
+            {/* <div></div> */}
           </div>
           <div className="event-item-footer">
-              <JoinOrNot event={event}/>
+            <JoinOrNot event={event} />
+            <span
+              style={{ width: "60%" }}
+              onClick={(e) => this.openEventModal(e)}
+            ></span>
           </div>
         </li>
       );
-    }else {
-      return (<div></div>)
+    } else {
+      return <div></div>;
     }
   }
 }
